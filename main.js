70af8551dc95a6223a83998a9aeb21f09f426c09
Oluwafemi <olukoyaoluwafemi14@gmail.com>
2022-02-01 13:23:55 +0100

    color for buttons

---
+++ main.js
@@ -0,0 +1,13 @@
+const btn = document.querySelectorAll('button');
+const result = document.getElementById('input_field');
+
+let value = '';
+console.log(result)
+// btn.forEach(buttons => {
+//     buttons.addEventListener('click',(e)=>{
+//         e.preventDefault();
+//         value += e.target.value;
+//         console.log(result)
+//     });
+
+// });
