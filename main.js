0c8f911591660b575270512d45c730e9f6cb944c
Oluwafemi <olukoyaoluwafemi14@gmail.com>
2022-02-05 23:28:17 +0100

    theme configuration

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
 const btn = document.querySelectorAll('button');
 const result = document.getElementById('input_field');
 
-let value = '';
-console.log(result)
-// btn.forEach(buttons => {
-//     buttons.addEventListener('click',(e)=>{
-//         e.preventDefault();
-//         value += e.target.value;
-//         console.log(result)
-//     });
+const theme = document.getElementById('body');
+const equal = document.getElementById('equal');
+const reset = document.getElementById('reset');
 
-// });
+let value = '';
+btn.forEach(buttons => {
+    buttons.addEventListener('click',(e)=>{
+        e.preventDefault();
+        value += e.target.value;
+        console.log(value)
+    });
+
+});
+
+equal.addEventListener('click',(e)=>{
+    e.preventDefault();
+   if(theme.classList.contains('home')){
+       theme.classList.remove('home');
+       theme.classList.add('theme-2')
+   } else{
+      
+   }
+    
+});
+
+reset.addEventListener('click',(e)=>{
+    e.preventDefault();
+   if(theme.classList.contains('theme-2')){
+       theme.classList.remove('theme-2');
+       theme.classList.add('theme-3')
+   } else{
+      
+   }
+    
+});
+
