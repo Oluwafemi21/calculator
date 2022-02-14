b488b388467fe183064eb49be09d28ae84c2ce55
Oluwafemi <olukoyaoluwafemi14@gmail.com>
2022-02-14 21:28:25 +0100

    new commit

--- main.js
+++ main.js
@@ -1,39 +1,54 @@
 const btn = document.querySelectorAll('button');
 const result = document.getElementById('input_field');
-
-const theme = document.getElementById('body');
+const inputArea = document.getElementById('field');
 const equal = document.getElementById('equal');
 const reset = document.getElementById('reset');
 
+/* Theme Changer
+ Get the buttons
+ Add event listener for each button
+ remove active class from all
+ then add
+ change visibility
+
+*/
+
+const themes = document.querySelectorAll('.items button');
+// const circles = document.querySelectorAll('.rounded span');
+
+themes.forEach((theme)=>{
+    theme.addEventListener('click',(e)=>{
+        e.preventDefault();
+        themes.forEach(theme => theme.classList.remove('active-btn'));
+
+        theme.classList.add('active-btn');
+
+        if(theme.id === 'theme2'){
+            body.classList.remove(body.classList);
+            body.classList.add('theme-2');
+        } else if(theme.id === 'theme3'){
+            body.classList.remove(body.classList);
+            body.classList.add('theme-3');
+        } else{
+            body.classList.remove(body.classList);
+            body.classList.add('home');
+        }
+    })
+})
+
+// circles.forEach((circle) =>{
+//     circles.forEach(circle => circle .classList.remove('active'));
+//     circle.classList.add('active');
+// })
+
 let value = '';
 btn.forEach(buttons => {
-    buttons.addEventListener('click',(e)=>{
-        e.preventDefault();
-        value += e.target.value;
-        console.log(value)
-    });
+   buttons.addEventListener('click',(e)=>{
+       e.preventDefault();
+       value += e.target.value;
+       console.log(inputArea.value);
+       console.log(value)
+   });
 
-});
-
-equal.addEventListener('click',(e)=>{
-    e.preventDefault();
-   if(theme.classList.contains('home')){
-       theme.classList.remove('home');
-       theme.classList.add('theme-2')
-   } else{
-      theme.classList.add('theme-2');
-   }
-    
-});
-
-reset.addEventListener('click',(e)=>{
-    e.preventDefault();
-   if(theme.classList.contains('theme-2')){
-       theme.classList.remove('theme-2');
-       theme.classList.add('theme-3')
-   } else{
-      theme.classList.add('theme-3');
-   }
-    
 });
 
